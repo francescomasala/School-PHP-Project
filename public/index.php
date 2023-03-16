90795be5b137daa1a921ee4aa233cec0d428a08f
Francesco Masala <mail@francescomasala.me>
2023-03-16 11:54:26 +0100

    Fixed index, header and footer

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ $test = new Test();
         <div class="col-12">
             <h1>Test</h1>
             <p><?= $test->testClass(); ?></p>
+            <a href="auth/signin.php">Login</a>
+            <a href="auth/signup.php">Registrati </a>
         </div>
     </div>
 </div>
--- snippets/header.php
+++ snippets/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <title>PHP - Masala</title>
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
-    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="https://unpkg.com/alpinejs" defer></script>
 </head>
 <style>
     :root {
