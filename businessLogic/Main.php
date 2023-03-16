7115e42482eedbb4c7024d546294e1ebbd297e6d
Francesco Masala <mail@francescomasala.me>
2023-03-16 11:55:08 +0100

    Added example function for generating a UUID

--- businessLogic/Main.php
+++ businessLogic/Main.php
@@ -1,12 +1,12 @@
 <?php
 
 include '../config.php';
-
+include '../businessLogic/Generators.php';
 class Test
 {
     public static function testClass(): string
     {
-        return "Hey! I'm working! :D";
+        return "Hey! I'm working! :D".Generators::generateUUID();
     }
 
 }
