c14aac490bdf8f8cd2537304b40586867a4e9fab
Francesco Masala <mail@francescomasala.me>
2023-03-16 11:53:56 +0100

    Added a couple of placeholder classes

---
+++ businessLogic/UserManagement.php
@@ -0,0 +1,18 @@
+<?php
+
+class UserManagement
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function registerUser(){
+
+    }
+
+    public static function loginUser(){
+
+    }
+
+}
