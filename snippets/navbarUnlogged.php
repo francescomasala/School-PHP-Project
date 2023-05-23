38065efa3f93bf87d0c9d15b0e04d049c9761563
Francesco Masala <mail@francescomasala.me>
2023-05-23 11:16:42 +0200

    Updated navbar

--- snippets/navbarUnlogged.php
+++ snippets/navbarUnlogged.php
@@ -1,10 +1,17 @@
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center mb-5 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <span class="fs-4">TechLab Manager</span>
-    </a>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+            <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+                <span class="fs-4 justify-content-sm-start">TechLab Manager</span>
+            </a>
+            </div>
 
-    <div class="col-md-3 text-end">
-        <a href="/auth/signin.php" class="btn btn-outline-primary me-2">Login</a>
-        <a href="/auth/signup.php" class="btn btn-primary">Registrati</a>
+            <div class="col-6 text-end">
+                <a href="/auth/signin.php" class="btn btn-outline-primary me-2">Login</a>
+                <a href="/auth/signup.php" class="btn btn-primary">Registrati</a>
+            </div>
+        </div>
+
     </div>
 </header>
